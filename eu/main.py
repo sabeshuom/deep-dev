057e12f1ca86f0dbcd52b0d0eec4a0202f397966
sabeshuom <sabesan.sivapalan@trademark.vision>
2016-03-10 09:21:06 +1000

    script to produce test train data for EU

--- eu/main.py
+++ eu/main.py
@@ -2,83 +2,95 @@
 import csv
 import json
 import get_data as gd
+import os
 
-root = 'tmv'
-src_dir = '/home/sabesan/deep-tmv'
-tmv_dir = src_dir + '/tmv'
 host = 'http://h17.ceeqapp.com:9250'
 es_type = 'trademark'
 es_index = 'EU'
-tmkeys_json = tmv_dir + '/logomatch/www/js/tmKeys_' + es_index + '.json'
-files = {'train': root + '_' + es_index + '_' + 'train.json', \
-         'validation': root + '_' + es_index + '_' + 'validation.json',\
-         'test': root + '_' + es_index + '_' + 'test.json'}
+prefix = 'tmv'
+
+home_dir = '/Volumes/DL'
+tmkeys_json = home_dir + '/tmv/logomatch/www/js/tmKeys_' + es_index + '.json'
+
+files = {'train': prefix + '_' + es_index + '_' + 'train.json', \
+         'validation': prefix + '_' + es_index + '_' + 'validation.json',\
+         'test': prefix + '_' + es_index + '_' + 'test.json',\
+         'map_key': prefix + '_'+ es_index + '_' + 'map_key.json'}
 key_start = 1
-key_size = 10
-no_of_classes = 100
-train_r  = 1.0
+key_size = 500000
+train_r  = 0.6
 test_r = 0.2
-# get the keywords for each image 
-#data = get_keywords(host, es_index, es_type, es_from, es_size)
+minNoOfImagesPerKey = 1000
 
-#save them as json file
-#save_keyidpair_as_json(data)
-
-#save json file as csv file
-
-#get the keyword pair with discritpions to count how many keyword pair
 tmkeys = gd.get_tmkeys(tmkeys_json)
 data = gd.get_keyword(host, es_type, es_index, key_start, key_size)
 
-map_key = {}
+map_key = []
 train = {}
-
-a = [{'id':34343} ,{'id':343434}]
-print a['id']
+test = {}
+validation = {}
 
 for k in sorted(data, key=lambda k: len(data[k]), reverse=True):
     ids  = data[k]
-    train_ids = ids[0:int((len(ids)*train_r))]
+    print k + ':' + str(len(ids)) 
     
-    #test_ids = ids((int((len(ids)*train_r)) +1):int((len(ids)* test_r)))
-    #validation_ids = ids(1:int((len(ids)*test_r)))
-    map_key[k] = len(map_key)
-  
-    for id in train_ids:
-        if 'id' in train:
-            if id in train['id']:
-                temp = train['id']
-                #print temp
-                #print "\n"
-                #print temp.index(id)
-                #print temp(temp.index(id))
-                #train[temp.index(id)]['key'].append(map_key[k])
-            else:
-                train['id'] = id
-                train['key'] = map_key[k]
+    if len(ids) < minNoOfImagesPerKey :
+        break
+    
+    train_lastidx = int((len(ids)*train_r))
+    test_lastidx = train_lastidx + int((len(ids)*test_r))
+    train_ids = ids[0:train_lastidx]
+    test_ids = ids[train_lastidx: test_lastidx]
+    validation_ids = ids[test_lastidx:]
+    d = {}
+    d['label'] = len(map_key)
+    d['key'] = k
+    d['n_tain'] = len(train_ids)
+    d['n_test'] = len(test_ids)
+    d['n_validation'] = len(validation_ids)
+    map_key.append(d)
+    
+    for _id in train_ids:
+        #print _id
+        if _id in train:
+            train[_id].append(d['label'])
         else:
-            train['id'] = id
-            train['key'] = map_key[k]
+            train[_id] = [d['label']]
 
-#print train
+    for _id in test_ids:
+        #print _id
+        if _id in test:
+            test[_id].append(d['label'])
+        else:
+            test[_id] = [d['label']]
 
+    for _id in validation_ids:
+        #print _id
+        if _id in validation:
+            validation[_id].append(d['label'])
+        else:
+            validation[_id] = [d['label']]
 
-#for key in data:
-#    print key
-#    print data[key]
+with open(files['train'], "w") as fp:
+    json.dump(train, fp, indent=4, sort_keys=True)
 
+print "train json saved"
+print "train path : " + os.path.abspath(files['train'])
 
+with open(files['test'], "w") as fp:
+    json.dump(test, fp, indent=4, sort_keys=True)
 
-#for key in len(data)
-#    data[key]['keyw']
-#    code[key].append()
+print "test json saved"
+     
+with open(files['validation'], "w") as fp:
+    json.dump(validation, fp, indent=4, sort_keys=True)
 
-#with open('test.json', "w") as fp:
-#    json.dump(data, fp, indent=4)
-#ids = gd.get_idsForkey(tmkeys[0])
+print "validation json saved"
 
-#for key in range(len(tmkeys))
-#trainin_data = gd.ids_forkeyword()
+with open(files['map_key'], 'w') as fp:  
+    json.dump(map_key, fp, indent=4, sort_keys=True)
+print "map key saved"
+
 
 
 
